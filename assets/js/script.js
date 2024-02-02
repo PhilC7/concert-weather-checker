e7cf574363213a629fbbe1739e896b19cfa70dce
Matthew Barker <matthewbarker@Rachels-MacBook-Air.local>
2024-02-02 11:56:19 +0000

    added logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,22 +5,22 @@ console.log("Hello");
     
 $(document).ready(function() {
     $("#my-form").submit(function(event) {
-      event.preventDefault();
-      var currentCity = $("#input_name").val();
-      console.log("Input Value:", currentCity);
-
-      
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=991cf6bd9810f1ee84be3949f37be642`)
-      .then(function (response) {
-          return response.json();
+        event.preventDefault();
+        var currentCity = $("#input_name").val();
+        console.log("Input Value:", currentCity);
+        
+        
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=991cf6bd9810f1ee84be3949f37be642`)
+        .then(function (response) {
+            return response.json();
         })
-          .then(function (data) {
+        .then(function (data) {
             $('#weatherEl').append('<h1>').text(data.name).attr('id', 'cityName');
-
+            
             $('#tempEl').append('<h5>').text(Math.round(data.main.temp - 274.15) + ' °C').attr('id', 'info');
-
+            
             $('#skysEl').append('<h5>').text(data.weather[0].main).attr('id', 'info');
-      });
+        });
     });
 });
         
