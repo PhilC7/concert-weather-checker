b56316626e06213c1631889c886c67379ffc5093
Phil Cooper <philjcooper@outlook.com>
2024-02-08 17:16:40 +0000

    refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,91 +1,71 @@
 $(document).ready(function () {
 
-    /********************
-     APIS
-    ********************/
-    var weatherKey = "7a91cf58326cd6da40050bda09317cd6";
-    var eventAPI = 'a1xNaECtJW1ZqG3CbA6rC75l1QJMc15f'
+/********************
+APIS
+********************/
+var weatherKey = "7a91cf58326cd6da40050bda09317cd6";
+var eventAPI = "a1xNaECtJW1ZqG3CbA6rC75l1QJMc15f";
 
 
-    /********************
-     GLOBAL VARIABLES
-    ********************/
-    var artistName = '' //change to take input value.
-    var history = [];
+/********************
+GLOBAL VARIABLES
+********************/
+var artistName = ''; //change to take input value.
+var history = []; //declate empty array for local storage
 
-    //get current day and format it
-    var date = dayjs().format("DD/MM/YYYY");
+/********************
+Display Event Function
+********************/
+function displayEvent(e) {
 
-    /********************
-    Display Event Function
-    ********************/
-    function displayEvent(e) {
+    $("#show-event").empty();
+    // set value of artists name for the search
+    artistName = $("#input_name").val() || artistName;
+    artist = artistName.replace(/\s/g, ''); //ignore all spacing from user input
 
-        $("#show-event").empty();
-        // set value of artists name for the search
-        artistName = $("#input_name").val() || artistName;
-        artist = artistName.replace(/\s/g, '');
+    // fetch data from ticket master api then weather api
+    var eventURL = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=GB&keyword=${artist}&apikey=${eventAPI}`;
+    fetch(eventURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (eventData) {
 
-        // fetch data from ticket master api then weather api
-        var eventURL = `https://app.ticketmaster.com/discovery/v2/events.json?countryCode=GB&keyword=${artist}&apikey=${eventAPI}`;
-        fetch(eventURL)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (eventData) {
-                // console.log(eventURL);
-                // console.log(artistName);
+            //get event location
+            var eventCity = eventData._embedded.events[2]._embedded.venues[0].city.name;
 
-                //get event location
-                var eventCity = eventData._embedded.events[2]._embedded.venues[0].city.name;
-                // console.log(eventCity);
+            // get event venue
+            var eventVenue = eventData._embedded.events[2]._embedded.venues[0].name;
 
-                // get event venue
-                var eventVenue = eventData._embedded.events[2]._embedded.venues[0].name;
-                // console.log(eventVenue);
+            // get event date
+            var eventDate = eventData._embedded.events[2].dates.start.localDate;
+            var formatDay = dayjs(eventDate).format("DD/MM/YYYY"); // format the event date
 
-                // get event time
-                var eventTime = eventData._embedded.events[2].dates.start.localTime;
-                var formatTime = dayjs(eventTime).format("hh A"); //format event time
-                // console.log(eventTime);
+            // get image of artist
+            var artistImg = eventData._embedded.events[2].images[4].url;
 
-                // get event date
-                var eventDate = eventData._embedded.events[2].dates.start.localDate;
-                var formatDay = dayjs(eventDate).format("DD/MM/YYYY"); // format the event date
-                // console.log(formatDay);
+            // fetch data for today's forecast using location from event location
+            var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${eventCity}&appid=${weatherKey}`;
+            fetch(weatherURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
 
-                // get image of artist
-                var artistImg = eventData._embedded.events[2].images[4].url;
-                // console.log(artistImg);
+                    // get temp
+                    var weatherTemp = Math.floor(data.main.temp - 273.15);
 
+                    // get weather code
+                    var weatherCode = data.weather[0].icon;
 
-                // fetch data for today's forecast using location from event location
-                var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${eventCity}&appid=${weatherKey}`;
-                fetch(weatherURL)
-                    .then(function (response) {
-                        return response.json();
-                    })
-                    .then(function (data) {
-                        console.log(weatherURL);
+                    // get weather description
+                    var weatherDescription = data.weather[0].description;
 
-                        // get temp
-                        var weatherTemp = Math.floor(data.main.temp - 273.15);
-                        // console.log(weatherTemp);
+                    // get weather icon from code
+                    var weatherIcon = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
 
-                        // get weather code
-                        var weatherCode = data.weather[0].icon;
-                        // console.log(weatherCode);
-
-                        // get weather description
-                        var weatherDescription = data.weather[0].description;
-                        // console.log(weatherDescription);
-
-                        // get weather icon from code
-                        var weatherIcon = `https://openweathermap.org/img/wn/${weatherCode}@2x.png`;
-                        // console.log(weatherIcon);
-
-                        $("#show-event").append(`
-                        <div class="card mb-6 col-lg-12">
+                    $("#show-event").append(`
+                    <div class="card mb-6 col-lg-12">
                         <div class="row g-0">
                             <div class="col-md-6">
                                 <img class="img-fluid rounded-md-start rounded-sm-top w-100" src="${artistImg}" alt="artist-image">
@@ -101,88 +81,86 @@ $(document).ready(function () {
                                     
                                 </div>
                             </div>
-
                         </div>
-                        </div>`)
+                    </div>`)
+                });
+        });
+}
 
-                    });
-            });
 
-
+/********************
+Display History Function
+********************/
+function displayHistory() {
+    $("#show-history").empty() //clear history to avoid duplicate cards
+    var history = localStorage.getItem("artists");
+    if (history == null || history == "") {
+        return;
     }
 
+    var historyBtn = JSON.parse(history); //parse local storage info
 
-    /********************
-  Display History Function
-  ********************/
-    function displayHistory() {
-        $("#show-history").empty() //clear history to avoid duplicate cards
-        var history = localStorage.getItem("artists");
-        if (history == null || history == "") {
-            return;
-        }
-
-        var historyBtn = JSON.parse(history);
-
-        // create card for each item of the array
-        for (let i = 0; i < historyBtn.length; i++) {
-            $("#show-history").append(`
-            <div class="col-lg-4 col-md-6 col-sm-12 d-flex align-items-stretch">
-                <div class="card w-100 history-card">
-                    <div class="card-body d-flex flex-column" id="historyCardTitle">
-                    <h5 class="card-title mb-3">${historyBtn[i]}</h5>
-                        <button id="reload" class="btn btn-primary mt-auto">Reload Event</button>
-                </div>
+    // create card for each item of the array
+    for (let i = 0; i < historyBtn.length; i++) {
+        $("#show-history").append(`
+        <div class="col-lg-4 col-md-6 col-sm-12 d-flex align-items-stretch">
+            <div class="card w-100 history-card">
+                <div class="card-body d-flex flex-column" id="historyCardTitle">
+                <h5 class="card-title mb-3">${historyBtn[i]}</h5>
+                    <button id="reload" class="btn btn-primary mt-auto">Reload Event</button>
             </div>
-        </div>`)
-            console.log(historyBtn);
-        }
+        </div>
+    </div>`)
     }
-    /********************
-    Clear History Function
-    ********************/
-    function clearHistory() {
-        localStorage.clear();
-        $("#show-history").empty(); // clear history cards
-        $("#show-event").empty(); // clear event card
-
-    }
-
-    // Event listener for clear history button
-    $("#clearHistoryBtn").on("click", clearHistory);
+}
 
 
-    $("#submit").on("click", function (event) {
-        event.preventDefault();
-        displayEvent();
-    });
+/********************
+Clear History Function
+********************/
+function clearHistory() {
+    localStorage.clear(); // clear localStorage
+    $("#show-history").empty(); // clear history cards
+    $("#show-event").empty(); // clear event card
+
+}
 
 
-    // click event to add information entered in the search input field to localStorage.
-    $("#submit").on("click", function (event) {
-        event.preventDefault();
-        var artist = $("#input_name").val().trim(); //set value based on user input
-        history.push(artist) //push that name to the history array
-        var stringifiedHistory = JSON.stringify(history); //stringify array for localStorage
-        $("#input_name").val(""); //reset input value to blank.
-        localStorage.setItem("artists", stringifiedHistory); //set localStorage key:value pairing.
+/********************
+Click Events
+********************/
 
-        displayHistory();
+// Event listener for clear history button
+$("#clearHistoryBtn").on("click", clearHistory);
 
-    });
+// Event to display current search
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    displayEvent();
+});
 
-    $("#show-history").on("click", "#reload", function (e) {
-        e.stopPropagation();
-        artistName = $(this).siblings(".card-title").text();
-        // console.log($(this).text());
-        displayEvent();
-    })
-
+// click event to add information entered in the search input field to localStorage.
+$("#submit").on("click", function (event) {
+    event.preventDefault();
+    var artist = $("#input_name").val().trim(); //set value based on user input
+    history.push(artist) //push that name to the history array
+    var stringifiedHistory = JSON.stringify(history); //stringify array for localStorage
+    $("#input_name").val(""); //reset input value to blank.
+    localStorage.setItem("artists", stringifiedHistory); //set localStorage key:value pairing.
     displayHistory();
 
+});
 
+// click event for each history card to display stored data.
+$("#show-history").on("click", "#reload", function (e) {
+    e.stopPropagation();
+    artistName = $(this).siblings(".card-title").text();
+    displayEvent();
+})
 
-    // bottom of document.ready function.
+displayHistory();
+
+// bottom of document.ready function.
 });
 
 
